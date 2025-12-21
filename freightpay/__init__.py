f2cbe84bcb6e9b410500fe17d7fbb37ce823b1ba
120mgmt <info@120mgmt.com>
2025-12-21 14:49:14 -0600

    Update __init__.py

--- freightpay/__init__.py
+++ freightpay/__init__.py
@@ -1,7 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 def create_app():
     app = Flask(__name__)
     @app.route("/")
     def home():
         return "FreightPay is running"
+    @app.route("oauth/gusto/callback")
+    def gusto_callback():
+        code = request.args.get("c0de")
+        return f"Gusto callback received. Code: {code}"
     return app
+    
