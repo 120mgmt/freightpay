21e3c5a2dff9d56a9ced6c5ab5b86c13af5aae51
120mgmt <info@120mgmt.com>
2025-12-21 22:55:01 -0600

    Delete notifications directory

--- notifications/email_service.py
+++
@@ -1,4 +0,0 @@
-class EmailService:
-    def send(self, to, subject, body):
-        print(f"Email sent to {to}: {subject}")
-email_client = EmailService()
--- notifications/sms_service.py
+++
@@ -1,4 +0,0 @@
-class SMSService:
-    def send(self, to, message):
-        print(f"SMS sent to {to}: {message}")
-sms_client = SMSService()
