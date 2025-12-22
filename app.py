6d745ff2fa9f582077c65b93de085c05f3b8eec1
120mgmt <info@120mgmt.com>
2025-12-21 18:37:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
+from flask import redirect
+import os
+import urllib.parse
+
 @app.route("/oauth/gusto/login")
 def gusto_login():
-    import os
-    from urllib.parse import urlencode
-
     params = {
         "client_id": os.environ["GUSTO_CLIENT_ID"],
         "redirect_uri": "https://freightpay.onrender.com/oauth/gusto/callback",
         "response_type": "code",
-        "scope": "payroll:companies payroll:employees payroll:contractors",
+        "scope": "companies:read employees:read contractors:read"
     }
 
-    return redirect(
-        "https://api.gusto.com/oauth/authorize?" + urlencode(params)
-    )
+    url = "https://api.gusto.com/oauth/authorize?" + urllib.parse.urlencode(params)
+    return redirect(url)
