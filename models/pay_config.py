e383b47413651dcc531efc8d77fecd08cdfeeff8
120mgmt <info@120mgmt.com>
2025-12-24 08:07:41 -0600

    Update pay_config.py

--- models/pay_config.py
+++ models/pay_config.py
@@ -1,1 +1,10 @@
-# place
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from .base import Base
+
+class PayConfig(Base):
+    __tablename__ = "pay_configs"
+
+    id = Column(Integer, primary_key=True)
+    driver_id = Column(Integer, ForeignKey("driver.id"), nullable=False)
+    pay_type = Column(string, nullable=False)
+    rate = Column(Float, nullable=False)
