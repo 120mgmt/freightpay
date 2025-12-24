7148c249109aab860abb435815dbae7f1d1eb5eb
120mgmt <info@120mgmt.com>
2025-12-24 00:42:54 -0600

    prod: fix config env var typo

--- src/config.py
+++ src/config.py
@@ -4,4 +4,4 @@ class Config:
     SECRET_KEY = os.getenv("SECRET-key", "change-me")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///freightpay.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", od.getenv("SECRET_KEY", "change-me"))
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", os.getenv("SECRET_KEY", "change-me"))
