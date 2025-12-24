879e64172fe337a83a5f3211f4fca0c4f424a2c0
120mgmt <info@120mgmt.com>
2025-12-24 01:10:06 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,8 @@
-# freightpay/config.py
-
+# config.py
 import os
 
-class BaseConfig:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SESSION_COOKIE_SECURE = True
-    SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SAMESITE = "Lax"
-    JSON_SORT_KEYS = False
 
-class ProductionConfig(BaseConfig):
-    ENV = "production"
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    PREFERRED_URL_SCHEME = "https"
+class Config:
+    APP_ENV = os.getenv("APP_ENV", "prod")
+    SECRET_KEY = os.getenv("SECRET_KEY", "change-me")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", os.getenv("SECRET_KEY", "change-me"))
