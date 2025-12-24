8f9d68945d2fbb5680328438b2914dd4e4d210ca
120mgmt <info@120mgmt.com>
2025-12-24 00:13:51 -0600

    prod: add config module

---
+++ src/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET-key", "change-me")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///freightpay.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", od.getenv("SECRET_KEY", "change-me"))
