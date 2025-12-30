04392be4bb07f1a9eb4328fce28563b39a88686c
120mgmt <info@120mgmt.com>
2025-12-29 21:00:31 -0600

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,20 @@
+# freightpay/models/__init__.py
+# Explicit model imports to ensure metadata registration on app startup
+
+from .base import Base
 from .company import Company
 from .driver import Driver
 from .pay_config import PayConfig
 from .payroll_run import PayrollRun
 from .settlement import Settlement
 from .ledger import LedgerEntry
+
+__all__ = [
+    "Base",
+    "Company",
+    "Driver",
+    "PayConfig",
+    "PayrollRun",
+    "Settlement",
+    "LedgerEntry",
+]
