72b203804dd27b9eac2a8205f0eb12f6065fa639
120mgmt <info@120mgmt.com>
2025-12-30 07:26:07 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from billing.checkout import billing_bp
 from billing.customer_portal import portal_bp
 from billing.webhooks import webhook_bp
 from payroll.routes.payroll_routes import payroll_bp
-from freightpay.legal.routes.legal_routes import legal_bp
+from legal.routes.legal_routes import legal_bp
 from bookkeeping.routes import bookkeeping_bp
 from integrations.gusto.oauth import gusto_bp
 
