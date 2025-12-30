1018472c5fd2502c090187fa63d4aa7451dc045c
120mgmt <info@120mgmt.com>
2025-12-29 20:58:27 -0600

    Update base.py

--- models/base.py
+++ models/base.py
@@ -1,5 +1,17 @@
+# freightpay/models/base.py
+
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import MetaData
 
-metadata = MetaData()
+# Naming convention required for reliable migrations (Alembic / prod-safe)
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = MetaData(naming_convention=NAMING_CONVENTION)
+
 Base = declarative_base(metadata=metadata)
